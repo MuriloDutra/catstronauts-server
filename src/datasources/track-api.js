aa46234aee52c3ea35af142f8105624714069816
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-02-03 14:20:33 -0300

    Creating a separate repository for server

---
+++ src/datasources/track-api.js
@@ -0,0 +1,30 @@
+const {RESTDataSource} = require("apollo-datasource-rest")
+
+class TrackAPI extends RESTDataSource {
+    constructor() {
+        super()
+        this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/"
+    }
+
+    getTracksForHome(){
+        return this.get("tracks")
+    }
+
+    getAuthor(authorId) {
+        return this.get(`author/${encodeURIComponent(authorId)}`)
+    }
+    
+    getTrack(trackId){
+        return this.get(`track/${encodeURIComponent(trackId)}`)
+    }
+
+    getTrackModules(trackId){
+        return this.get(`track/${encodeURIComponent(trackId)}/modules`)
+    }
+
+    incrementTrackViews(trackId){
+        return this.patch(`track/${encodeURIComponent(trackId)}/numberOfViews`)
+    }
+}
+
+module.exports = TrackAPI
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require("apollo-server");
+const TrackAPI = require("./datasources/track-api");
+const resolvers = require("./resolvers");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI(),
+    };
+  },
+});
+
+server.listen().then(() => {
+  console.log(`
+      🚀  Server is running!
+      🔉  Listening on port 4000
+      📭  Query at http://localhost:4000
+    `);
+});
